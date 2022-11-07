963dcd6b30ec8aab3e6fcee705ef6f27c65093db
corpp02535 <brunohorn@ext.cvccorp.com.br>
2022-11-06 21:55:21 -0300

    routes config mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,8 +7,8 @@ import {
   Roboto_500Medium,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
-import { SignIn } from './src/screens/SignIn';
 import { AuthContextProvider } from './src/contexts/AuthContext';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +25,7 @@ export default function App() {
           backgroundColor='transparent'
           translucent
         />
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
